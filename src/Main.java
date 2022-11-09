4fec1c0960726ee9621e671b07ee29b920dd95a0
tokrist <68947997+tokrist@users.noreply.github.com>
2022-11-09 09:28:24 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author tokri
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner("Adjon meg az elso szamot!");
+        double number1 = input.nextInt();
+        input = new Scanner("Adja meg a masodik szamot!");
+        double number2 = input.nextInt();
+        
+        
+        System.out.println("A tergogat: " + terfogatSzamol(number1, number2));
+        System.out.println("A felszin: " + felszinSzamol(number1, number2));
+    }
+    
+}
